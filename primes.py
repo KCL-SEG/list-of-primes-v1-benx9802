4b0bf26c4a6c45905ee8ec6da1fafa2c8807a02f
benx9802 <benjamin.ferrey@kcl.ac.uk>
2023-09-29 20:39:39 +0100

    solution

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    x = 2
+    while len(list) < number_of_primes:
+        if ifprime(x):
+            list.append(x)
+        x = x+1
     return list
+
+def ifprime(num):
+    for i in range(2,num): # for all possible factors of i
+        if num % i == 0: # if it is a factor
+            return False
+    return True
